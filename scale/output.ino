4656e60d26f1c986766555c952c2c7f0516c9484
d-tilley <david@tilleytap.com>
2017-01-24 22:47:21 +0000

    HX711 readers

---
+++ scale/output.ino
@@ -0,0 +1,51 @@
+/*
+ Example using the SparkFun HX711 breakout board with a scale
+ By: Nathan Seidle
+ SparkFun Electronics
+ Date: November 19th, 2014
+ License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
+ 
+ Most scales require that there be no weight on the scale during power on. This sketch shows how to pre-load tare values
+ so that you don't have to clear the scale between power cycles. This is good if you have something on the scale 
+ all the time and need to reset the Arduino and not need to tare the scale.
+ 
+ This example code uses bogde's excellent library: https://github.com/bogde/HX711
+ bogde's library is released under a GNU GENERAL PUBLIC LICENSE
+ 
+ The HX711 does one thing well: read load cells. The breakout board is compatible with any wheat-stone bridge
+ based load cell which should allow a user to measure everything from a few grams to tens of tons.
+ Arduino pin 2 -> HX711 CLK
+ 3 -> DOUT
+ 5V -> VCC
+ GND -> GND
+ 
+ The HX711 board can be powered from 2.7V to 5V so the Arduino 5V power should be fine.
+ 
+*/
+
+#include "HX711.h"
+
+#define calibration_factor -6470 //This value is obtained using the SparkFun_HX711_Calibration sketch
+#define zero_factor 180954 //This large value is obtained using the SparkFun_HX711_Calibration sketch
+
+#define DOUT 3
+#define CLK 2
+
+HX711 scale(DOUT, CLK);
+
+void setup() {
+  Serial.begin(9600);
+
+  scale.set_scale(calibration_factor);
+  scale.set_offset(zero_factor); //Zero out the scale using a previously known zero_factor
+}
+
+void loop() {
+  
+  scale.power_up();
+  
+  Serial.println(scale.get_units(), 2); //scale.get_units() returns a float
+  
+  scale.power_down();
+  delay(5000);
+}
