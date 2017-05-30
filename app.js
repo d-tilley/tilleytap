09892cdd633642c5baee588e279b56b9f1bf5aa3
David Tilley <tilley13k@gmail.com>
2017-05-30 12:49:53 +0000

    add mongodb

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 const bodyParser= require('body-parser');
+
 const app = express();
+
+var db;
+
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/tilleytap", function(err, database) {
+    if (err) return console.log(err);
+    
+    app.listen(8080, function() {
+      console.log('listening on 8080')
+    });
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(8080, function() {
-  console.log('listening on 8080')
+app.get('/', function(req, res) {
+    res.send('Hello World')
 });
 
-app.get('/', function(req, res) {
-  res.send('Hello World')
+app.get('/test', function(req, res) {
+    db.collection('test').find().toArray(function (err, docs) {
+        if (err) return console.log(err);
+        
+        console.log('docs: ' + docs);
+        res(docs);
+    });
 });
