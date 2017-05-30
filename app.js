069f84d15fa9366e496791166c0ffcfd7efbce99
David Tilley <tilley13k@gmail.com>
2017-05-30 02:35:18 +0000

    add body-parser dependency

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const bodyParser= require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(8080, function() {
   console.log('listening on 8080')
-})
+});
 
 app.get('/', function(req, res) {
   res.send('Hello World')
-})
+});
